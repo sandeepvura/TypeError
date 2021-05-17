7ce055dd4badfc1fd15ed6d848ca8307423b8c20
sandeepvura <>
2021-05-17 19:38:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+name = len(input("what is your name?"))
+
+new_name = str(name)
+##Type conversion
+print("your name has " + new_name + " characters")
+
+# Note: You can convert Int to String . Once it is converted you can concatenate.
+
+
